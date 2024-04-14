520522607b89803c030387afa4f3a76902f2c7a8
Abubakar1010 <abubakar850772@gmail.com>
2024-04-14 12:47:21 +0600

    finished project setup with react tailwind css and material tailwind

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes/Router'
+import { ThemeProvider } from '@material-tailwind/react'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider>
+    <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
