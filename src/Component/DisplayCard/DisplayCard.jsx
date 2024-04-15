bd7b6e81597cb8ed0c1e9e7e1a9e327e5d6f5228
Abubakar1010 <abubakar850772@gmail.com>
2024-04-15 19:44:30 +0600

    finished display estate card

---
+++ src/Component/DisplayCard/DisplayCard.jsx
@@ -0,0 +1,75 @@
+import { FaStar } from "react-icons/fa";
+import { BiDollar } from "react-icons/bi";
+import { IoLocation } from "react-icons/io5";
+import PropTypes from "prop-types"
+
+
+const DisplayCard = ({ element }) => {
+  console.log(element);
+
+  const { estate_title, image, location, price, status, rating, facilities } = element;
+  return (
+    <div className=" flex justify-center items-center h-[470px]">
+      <div className=" shadow-xl border-2 border-[#0003] h-full">
+        <div className=" ">
+          <img
+            src={image}
+            alt=""
+            className=" w-[540px] h-[240px] object-cover px-4 py-6"
+          />
+        </div>
+        <div className=" px-4 pb-6">
+          <div className=" flex items-center justify-between">
+            <h1 className=" font-bold font-PlayFair text-xl ">
+              {estate_title}
+            </h1>
+            <div className=" flex items-center gap-2">
+              <FaStar className=" text-xl text-orange-500" />
+              <p className=" font-medium text-lg ">{rating}</p>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className=" flex items-center flex-wrap px-4 pb-6">
+                    <p className=" text-lg font-bold ">Facilities:</p>
+                    {
+                        facilities.map( (element,index) => <p  key={index} className="px-2 border-black">#{element}</p>)
+                    }
+                </div> */}
+                <div className=" flex  items-center pb-4 px-4">
+                    <p className=" pr-2 text-[#000000de] font-PlayFair">{facilities[0]}</p>
+                    <p className=" border-2 border-[#000000a4] h-[16px]"></p>
+                    <p className="px-2 text-[#000000de] font-PlayFair">{facilities[1]}</p>
+                    <p className=" border-2 border-[#000000a4] h-[16px]"></p>
+                    <p className="pl-2 text-[#000000de] font-PlayFair">{facilities[2]}</p>
+                </div>
+        <div className=" flex justify-between px-4 pb-4">
+          <p className=" font-bold font-PlayFair text-xl">For {status}</p>
+          <div className=" flex items-center gap-1">
+            <BiDollar className=" text-xl font-extrabold" />
+            <p>{price}</p>
+          </div>
+        </div>
+
+        <div className=" flex justify-between px-3 items-center pt-4 pb-7">
+        <div className=" flex item-center gap-1">
+            <IoLocation className=" text-2xl text-[#FF8938] " />
+            <p>{location}</p>
+        </div>
+        <div>
+        <button className="hidden lg:inline-block bg-gradient-to-r from-[#ff0000] to-[#FF8938]  py-2 px-4 text-white font-bold font-PlayFair rounded-md text-xl hover:scale-105 duration-500 hover:duration-500  ">
+            View Details
+        </button>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+DisplayCard.propTypes = {
+
+    element: PropTypes.object
+}
+
+export default DisplayCard;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Banner from "../../Component/Banner/Banner";
+import Card from "../../Component/Card/Card";
+
+const Home = () => {
+    return (
+        <>
+            <div>
+                <Banner />
+                <Card />
+            </div>
+        </>
+    );
+};
+
+export default Home;
